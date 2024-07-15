d466c665e759daa0b712ad41bfe1866ef2d1b2cf
bagusyanuar <50134851+bagusyanuar@users.noreply.github.com>
2024-07-15 13:39:38 +0700

    database

--- database/migrations/2024_07_15_063920_create_member.php
+++ database/migrations/2024_07_15_063920_create_member.php
@@ -11,11 +11,8 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('members', function (Blueprint $table) {
             $table->id();
-            $table->string('username')->unique();
-            $table->string('password');
-            $table->string('role');
             $table->timestamps();
         });
     }
@@ -25,6 +22,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('members');
     }
 };
